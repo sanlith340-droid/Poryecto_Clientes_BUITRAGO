15fb2d642adeb414b044d4af928d45af501f8205
sanlith340-droid <sanlith340@gmail.com>
2026-07-01 20:39:07 -0500

    Separacion de modelos , y el id para la creacion

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
 from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel 
+from modelos.clientes import Cliente, ClienteCrear
 
 app = FastAPI()
 
 
-#**crear el modelo clientes(id, nombre, email, descripcion)** #Mayuscula incial para class
-class Cliente(BaseModel): 
-    id: int
-    nombre: str
-    email: str
-    descripcion: str
+
     
 #variables lista de clientes
 lista_clientes:list[Cliente] = [
@@ -24,13 +19,13 @@ lista_clientes:list[Cliente] = [
 
 #get: obtener o listar
 
-@app.get("/clientes")
+@app.get("/clientes",response_model=list[Cliente])
 def listar_clientes():
     return  lista_clientes
 
 #endpoint / para lisar un solo cliente  de la lista
 
-@app.get("/clientes{cliente_id}")
+@app.get("/clientes{cliente_id}",response_model=Cliente)
 def listar_clientes(cliente_id: int):
     
     #recorrer la lista clientes
@@ -40,7 +35,8 @@ def listar_clientes(cliente_id: int):
         
 #
 #endpoint / para crear  un cliente, y agregar a la lista
-@app.post("/clientes/")
-def crear_cliente(datos_cliente: Cliente):
-    lista_clientes.append(datos_cliente)
-    return datos_cliente
+@app.post("/clientes",response_model=Cliente)
+def crear_cliente(datos_cliente: ClienteCrear):
+    cliente_val = Cliente.model_validate(datos_cliente.model_dump())
+    lista_clientes.append(cliente_val)
+    return cliente_val
---
+++ modelos/clientes.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+#**crear el modelo clientes(id, nombre, email, descripcion)** #Mayuscula incial para class
+
+#seguridad a los modelos de datos
+
+class ClienteBase(BaseModel): 
+    nombre: str
+    email: str
+    descripcion: str
+    
+class ClienteCrear(ClienteBase):
+    pass
+    
+class Cliente(ClienteBase):
+    id: int | None = None 
