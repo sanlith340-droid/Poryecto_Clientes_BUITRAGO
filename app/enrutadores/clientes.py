0c129b509821e8916c78feb7da4cb30621e623cd
unknown <sanlith340@gmail.com>
2026-07-03 15:13:13 -0500

    Video_16

--- app/enrutadores/clientes.py
+++ app/enrutadores/clientes.py
@@ -29,6 +29,7 @@ async def ListarCliente(cliente_id: int, mi_sesion: sesion_dependencia):
         )
     return cliente_bd
         
+        
 #
 #endpoint / para crear  un cliente, y agregar a la lista
 @rutas_clientes.post("/clientes",response_model=Cliente)
@@ -41,21 +42,30 @@ async def crear_cliente(datos_cliente: ClienteCrear , mi_sesion: sesion_dependen
 
 #endpoint / para crear  un cliente, y agregar a la lista
 @rutas_clientes.patch("/clientes/{cliente_id}",response_model=Cliente)
-async def editar_cliente(cliente_id: int, datos_cliente: ClienteEditar):
-    for i , obj_cliente in enumerate (lista_clientes):
-        if obj_cliente.id == cliente_id:
-            #validar cliente
-            cliente_val = Cliente.model_validate(datos_cliente.model_dump())
-            cliente_val.id = cliente_id
-            lista_clientes[i] = cliente_val
-            return  cliente_val
-        raise HTTPException(status_code=400, detail=f"El cliente con id {cliente_id} , no existe")
+async def editar_cliente(cliente_id: int, datos_cliente: ClienteEditar, mi_sesion:sesion_dependencia):
+    cliente_bd = mi_sesion.get(Cliente, cliente_id)
+    if not cliente_bd:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Cliente con id {cliente_id} no encontrado"
+        )
+    cliente_dict = datos_cliente.model_dump(exclude_unset=True)
+    cliente_bd.sqlmodel_update(cliente_dict)
+    mi_sesion.add(cliente_bd)
+    mi_sesion.commit()
+    mi_sesion.refresh(cliente_bd)
+    return cliente_bd
+
 
 #endpoint / para eliminar  un cliente, y agregar a la lista
 @rutas_clientes.delete("/clientes/{cliente_id}",response_model=Cliente)    
-async def eliminar_cliente(cliente_id: int):
-    for i , obj_cliente in enumerate (lista_clientes):
-        if obj_cliente.id == cliente_id:
-            cliente_eliminado = lista_clientes.pop(i)
-            return cliente_eliminado
-    raise HTTPException(status_code=400, detail=f"El cliente con id {cliente_id} , no existe")
+async def EliminarCliente(cliente_id: int, mi_sesion: sesion_dependencia):
+    cliente_bd = mi_sesion.get(Cliente, cliente_id)
+    if not cliente_bd:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Cliente con id {cliente_id} no encontrado"
+        )
+    mi_sesion.delete(cliente_bd)
+    mi_sesion.commit()
+    return cliente_bd
